f8600484b74b161a34b4fa515dc578bc9ceb5159
Rukshani Athapathu <dathapathu@yeti-04.sysnet.ucsd.edu>
2022-07-22 11:03:21 -0700

    Support multiple memcached instances

--- scripts/memcached/start-memcached-server.sh
+++ scripts/memcached/start-memcached-server.sh
@@ -30,21 +30,26 @@ done
 # IFACE=ens1f0
 # nic_local_numa_node=$(cat /sys/class/net/$IFACE/device/numa_node)
 
-server_port_pair="$server:11211"
-for i in $(seq 1 $parallelism);
-do
-    port=$(echo "11211+$i" | bc)
-    current_server=$server
-    if [[ $USE_SEPARATE_SERVER -eq 1 ]]; then
+server_port_pair=''
+if [[ $USE_SEPARATE_SERVER != 1 ]]; then
+    server_port_pair="$server:11211"
+fi
+
+if [[ $USE_SEPARATE_SERVER -eq 1 ]]; then
+    for i in $(seq 1 $parallelism);
+    do
+        port=$(echo "11211+$i" | bc)
+        current_server=$server
         # echo >&2 "Using separate servers ..."
         IFS='.' read ip1 ip2 ip3 ip4 <<< "$server"
-        ip2=$(echo "1 + ($i - 1) / 32" | bc)
+        # ip2=$(echo "1 + ($i - 1) / 32" | bc)
         ip4=$(echo "$ip4 + ($i - 1) % 32 + 1" | bc)
         current_server="$ip1.$ip2.$ip3.$ip4"
         # echo >&2 "\t$i-th server: $current_server"
         server_port_pair+="$current_server:$port,"
-    fi
-done
+        
+    done
+fi
 
 echo $server_port_pair
 memcached -l $server_port_pair &
