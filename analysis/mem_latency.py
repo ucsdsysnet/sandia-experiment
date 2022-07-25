feeb0dbbac73479192aa2610aa79a3448f88acf4
Rukshani Athapathu <dathapathu@yeti-01.sysnet.ucsd.edu>
2022-07-25 13:52:43 -0700

    Load json

--- analysis/mem_latency.py
+++ analysis/mem_latency.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import tarfile
+import json
 
 DATAPATH_RAW="/home/dathapathu/experiments/data-tmp/"
 DATAPATH_PROCESSED="/home/dathapathu/experiments/data-processed/"
@@ -14,5 +15,18 @@ def load_experiments(experiment_name_pattern):
         my_tar = tarfile.open(local_filepath[0])
         my_tar.extractall(DATAPATH_PROCESSED)
         my_tar.close()
-    
-load_experiments("*25-07-2022_095308*")
+
+def get_summary(experiment_name_pattern, nic_type):
+    for x in range(101, 102):
+        json_file_name = "memt-" + nic_type + "-" + str(x) + "-" + experiment_name_pattern + ".json"
+        path_to_json = DATAPATH_PROCESSED + json_file_name
+        # print(path_to_json)
+        with open(path_to_json, 'r') as f:
+            data = json.load(f)
+            print(data['ALL STATS']['Gets']['Ops/sec'])
+            print(data['ALL STATS']['Gets']['Percentile Latencies']['p50.00'])
+            print(data['ALL STATS']['Gets']['Percentile Latencies']['p99.00'])
+            print(data['ALL STATS']['Gets']['Percentile Latencies']['p99.90'])
+
+# load_experiments("*25-07-2022_095308*")
+get_summary('25-07-2022_095308', 'cx5')
