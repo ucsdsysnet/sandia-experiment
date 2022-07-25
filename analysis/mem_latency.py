054cb5972a62988df9a83d26b7944f2eaecefc09
Rukshani Athapathu <dathapathu@yeti-01.sysnet.ucsd.edu>
2022-07-25 14:53:15 -0700

    Get summary stats

--- analysis/mem_latency.py
+++ analysis/mem_latency.py
@@ -2,6 +2,7 @@ import glob
 import os
 import tarfile
 import json
+import pandas as pd
 
 DATAPATH_RAW="/home/dathapathu/experiments/data-tmp/"
 DATAPATH_PROCESSED="/home/dathapathu/experiments/data-processed/"
@@ -17,16 +18,23 @@ def load_experiments(experiment_name_pattern):
         my_tar.close()
 
 def get_summary(experiment_name_pattern, nic_type):
-    for x in range(101, 102):
+    instance_summary = pd.DataFrame(columns=['nic_type', 'instance_name', 'ops_sec', 'p50', 'p99', 'p99_9'])
+    for x in range(101, 133):
         json_file_name = "memt-" + nic_type + "-" + str(x) + "-" + experiment_name_pattern + ".json"
         path_to_json = DATAPATH_PROCESSED + json_file_name
         # print(path_to_json)
         with open(path_to_json, 'r') as f:
             data = json.load(f)
-            print(data['ALL STATS']['Gets']['Ops/sec'])
-            print(data['ALL STATS']['Gets']['Percentile Latencies']['p50.00'])
-            print(data['ALL STATS']['Gets']['Percentile Latencies']['p99.00'])
-            print(data['ALL STATS']['Gets']['Percentile Latencies']['p99.90'])
+            flow_summary = {}
+            flow_summary['nic_type'] = nic_type
+            flow_summary['instance_name'] = nic_type + "-" + str(x) + "-" + experiment_name_pattern
+            flow_summary['ops_sec'] = data['ALL STATS']['Gets']['Ops/sec']
+            flow_summary['p50'] = data['ALL STATS']['Gets']['Percentile Latencies']['p50.00']
+            flow_summary['p99'] = data['ALL STATS']['Gets']['Percentile Latencies']['p99.00']
+            flow_summary['p99_9'] = data['ALL STATS']['Gets']['Percentile Latencies']['p99.90']
+            instance_summary = instance_summary.append(flow_summary, True)
+            # print(flow_summary)
+    print(instance_summary)
 
 # load_experiments("*25-07-2022_095308*")
 get_summary('25-07-2022_095308', 'cx5')
