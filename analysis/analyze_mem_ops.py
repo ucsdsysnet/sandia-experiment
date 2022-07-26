46e71e050cf38f697e2e10cc662e65e0bb573c4c
Rukshani Athapathu <dathapathu@yeti-01.sysnet.ucsd.edu>
2022-07-26 11:31:36 -0700

    Get Ops

--- analysis/analyze_mem_ops.py
+++ analysis/analyze_mem_ops.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import plotly
 import plotly.graph_objects as go
+import plotly.express as px
 
 BASE_PATH = "/home/dathapathu/experiments/"
 
@@ -11,16 +12,9 @@ cx5_c22_path = BASE_PATH + "data-summary/" + "summary-c22-cx5.csv"
 fpga_c11_path = BASE_PATH + "data-summary/" + "summary-c11-fpga.csv"
 fpga_c22_path = BASE_PATH + "data-summary/" + "summary-c22-fpga.csv"
 
-def plot_summary(fig, df_summary, identifier):
-    fig.add_trace(
-                go.Scatter(x=df_summary["instance_id"], y=df_summary["p50"], name=identifier + "-p50", mode='lines', text=df_summary["p50"])
-            )
-    fig.add_trace(
-                go.Scatter(x=df_summary["instance_id"], y=df_summary["p99"], name=identifier + "-p99", mode='lines', text=df_summary["p99"])
-            )
-    fig.add_trace(
-                go.Scatter(x=df_summary["instance_id"], y=df_summary["p99_9"], name=identifier + "-p99_9", mode='lines', text=df_summary["p99_9"])
-            )
+def plot_ops_summary(df):
+    fig = px.bar(df, x='type', y='Ops')
+    # fig.show()
     return fig
 
 def get_instance_id(instance):
@@ -40,20 +34,29 @@ def load_summary():
     fpga_c22 = pd.read_csv(fpga_c22_path, sep=',', header=0, names=["index", "nic_type", "instance_name", "ops_sec", "p50", "p99", "p99_9"])
     fpga_c22['instance_id'] = fpga_c22['instance_name'].apply(get_instance_id)
 
-    fig = go.Figure()
-    fig = plot_summary(fig, cx5_c11, "cx5_c11")
-    fig = plot_summary(fig, cx5_c22, "cx5_c22")
-    fig = plot_summary(fig, fpga_c11, "fpga_c11")
-    fig = plot_summary(fig, fpga_c22, "fpga_c22")
+    cx5_c11_ops = cx5_c11['ops_sec'].sum()
+    cx5_c22_ops = cx5_c22['ops_sec'].sum()
+    fpga_c11_ops = fpga_c11['ops_sec'].sum()
+    fpga_c22_ops = fpga_c22['ops_sec'].sum()
+
+    data_ops = {'type': ['cx5_c11_ops', 'cx5_c22_ops', 'fpga_c11_ops', 'fpga_c22_ops'], 'Ops': [cx5_c11_ops, cx5_c22_ops, fpga_c11_ops, fpga_c22_ops]}
+    df = pd.DataFrame(data=data_ops)
+    # print(df)
+    # data_ops = [[cx5_c11_ops, cx5_c22_ops, fpga_c11_ops, fpga_c22_ops]]
+    # df = pd.DataFrame(data_ops)
+    # df.columns =['cx5_c11', 'cx5_c22', 'fpga_c11', 'fpga_c22']
+
+    fig = plot_ops_summary(df)
 
     fig.update_layout(
-            title="p50, p99, p99.9 Percentile Latency - Yeti (CX5 and FPGA)",
-            xaxis_title="<b>Client Instance ID</b>",
-            yaxis_title="<b>Latency (milliseconds)</b>"
+            title="Memcached (All Instances) - Operations per Second - Yeti (CX5 and FPGA)",
+            xaxis_title="<b>NIC TYPE</b>",
+            yaxis_title="<b>Ops</b>"
             # yaxis={'tickformat': ',d'}
         )
 
-    filename_plot = DATAPATH_SUMMARY + "percentile_latency_summary.html"
+    filename_plot = DATAPATH_SUMMARY + "ops_summary.html"
     plotly.offline.plot(fig, filename=filename_plot)
+
 
 load_summary()
