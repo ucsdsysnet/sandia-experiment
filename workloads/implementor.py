5cec2ae8080b1d81ce5e3354b136758cc96cdf0c
rukshani <rukshani@localhost>
2022-08-11 14:37:25 -0600

    Automate test suit

--- workloads/implementor.py
+++ workloads/implementor.py
@@ -1,22 +1,28 @@
+from command import RemoteCommand, run_local_command, get_ssh_client, exec_command
+
 def start_iperf_server(exp_obj, exp_template, workload, stack):
     log_name = { 'iperf_server' : '/tmp/iperf-server-{}-r{}-{}.csv'.format(exp_obj.id, exp_obj.iteration, exp_obj.exp_time)}
     exp_obj.append_logs(log_name)
-    print(exp_obj.all_logs)
-    # start_server_cmd = ('iperf3 --server '
-    #                         '--bind {} '
-    #                         '--port {} '
-    #                         '--one-off '
-    #                         '--logfile {} ').format(
-    #                             experiment['server_list'][0],
-    #                             5100,
-    #                             flow.server_log)
-    # print("iperf server cmd>", start_server_cmd)
-    # start_server = cctestbed.RemoteCommand(start_server_cmd,
-    #                                     experiment.server.ip_wan,
-    #                                     username=experiment.server.username,
-    #                                     logs=[flow.server_log],
-    #                                     key_filename=experiment.server.key_filename)
-    # stack.enter_context(start_server())
+    print(log_name['iperf_server'])
+
+    start_server_cmd = ('iperf3 --server '
+                            '--bind {} '
+                            '--port {} '
+                            '--one-off '
+                            '--logfile {} ').format(
+                                exp_template['server_list'][0],
+                                5100,
+                                log_name['iperf_server'])
+
+    print("iperf server cmd>", start_server_cmd)
+
+    start_server = RemoteCommand(start_server_cmd,
+                                exp_template['server_list'][0],
+                                username=exp_template['username'],
+                                logs=[log_name['iperf_server']],
+                                key_filename=exp_template['key_filename'])
+
+    stack.enter_context(start_server())
 
 def start_iperf_clients(experiment, workload):
     print("start iperf clients: ")
