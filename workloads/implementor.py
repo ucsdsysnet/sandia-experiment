50a74b3f0db4e25d1b44f83d2e9db4747f480a29
rukshani <rukshani@localhost>
2022-08-14 10:28:53 -0600

    Run client as a local command

--- workloads/implementor.py
+++ workloads/implementor.py
@@ -1,5 +1,8 @@
 from command import RemoteCommand, run_local_command, get_ssh_client, exec_command
 import constant as c
+from contextlib import contextmanager, ExitStack
+import os
+import logging
 
 def start_iperf_server(exp_obj, exp_template, workload, stack):
     log_name = { 'iperf_server' : '/tmp/iperf-server-{}-r{}-{}.csv'.format(exp_obj.id, exp_obj.iteration, exp_obj.exp_time)}
@@ -24,6 +27,28 @@ def start_iperf_server(exp_obj, exp_template, workload, stack):
 
     stack.enter_context(start_server())
 
+@contextmanager
+def run_as_local(start_client_cmd):
+    pid = None
+    try:
+        os.system(start_client_cmd)
+        pid = run_local_command('pgrep -f "{}"'.format(start_client_cmd))
+        if (pid is None or pid == ""):
+            print("Iperf client PID is None or empty:", pid)
+        else:
+            print("Iperf client PID is: ", pid)
+            # assert(pid)
+            # pid = int(pid)
+            # logging.info('PID={}'.format(pid))
+            # yield pid
+        yield
+    except Exception as ex:
+        print(False)
+    # finally:
+        # print("finally---")
+        # logging.info('Cleaning up cmd: {}'.format(start_client_cmd))
+        # run_local_command('kill {}'.format(pid))
+
 def start_iperf_clients(exp_obj, exp_template, workload, stack):
     log_name = { 'iperf_client' : '/tmp/iperf-client-{}-r{}-{}.csv'.format(exp_obj.id, exp_obj.iteration, exp_obj.exp_time)}
     exp_obj.append_logs(log_name)
@@ -44,17 +69,18 @@ def start_iperf_clients(exp_obj, exp_template, workload, stack):
     print("iperf client cmd>", start_client_cmd)
 
     #If the client is the control machine run as a local command
-    
-
-    #Only when client is a remote machine
-    # start_client = RemoteCommand(
-    #         start_client_cmd,
-    #         exp_template['client_list_wan'][0],
-    #         username=exp_template['username'],
-    #         logs=[log_name['iperf_client']],
-    #         key_filename=exp_template['key_filename'])
-    
-    # stack.enter_context(start_client())
+    out_str = run_local_command('ifconfig | grep -w {}'.format(exp_template['client_list'][0]), True)
+    if out_str.find(exp_template['client_list'][0]) != -1:
+        stack.enter_context(run_as_local(start_client_cmd))
+    else:
+        #Only when client is a remote machine
+        start_client = RemoteCommand(
+                start_client_cmd,
+                exp_template['client_list_wan'][0],
+                username=exp_template['username'],
+                logs=[log_name['iperf_client']],
+                key_filename=exp_template['key_filename'])
+        stack.enter_context(start_client())
 
 def start_memcached_clients(experiment, workload):
     print("start memcached clients: ")
