acfd7d926c60cee1c5c163875e3bf92a3125ee8a
rukshani <rukshani@localhost>
2022-08-29 14:17:22 -0600

    worker setup

--- workloads/hibench/README.md
+++ workloads/hibench/README.md
@@ -1,4 +1,4 @@
-## Setup Hadoop bench in HiBench (On Master Node - Control node in this testsuite)
+## Setup Hadoop bench in HiBench (On Master Node - Control node in this test suite)
 ```
 ./workloads/hibench/setup_hibench.sh
 ```
--- workloads/hibench/setup_hibench_worker.sh
+++ workloads/hibench/setup_hibench_worker.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+. workloads/hibench/configurations.config
+MASTER_PUBLIC_IP=$master_public_ip
+
+sudo chown -R $USER $HOME
+
+# sudo apt-get update
+# sudo apt-get remove --purge openjdk-11-'*'
+# sudo apt-get install -y openjdk-8-jdk
+
+var=$HOME/sw
+mkdir -p $var
+cd $var
+
+# scp -r $USER@$MASTER_PUBLIC_IP:$HOME/sw/hadoop-3.2.4.tar.gz $HOME/sw
