b8fa760e0450e87cd60a614b23151598b81ac432
rukshani <rukshani@localhost>
2022-08-29 14:45:56 -0600

    Configure worker

--- workloads/hibench/setup_hibench_worker.sh
+++ workloads/hibench/setup_hibench_worker.sh
@@ -2,17 +2,17 @@
 
 sudo chown -R $USER $HOME
 
-sudo apt-get update
-sudo apt-get remove --purge openjdk-11-'*'
-sudo apt-get install -y openjdk-8-jdk
+# sudo apt-get update
+# sudo apt-get remove --purge openjdk-11-'*'
+# sudo apt-get install -y openjdk-8-jdk
 
-var=$HOME/sw
-mkdir -p $var
-cd $var
+# var=$HOME/sw
+# mkdir -p $var
+# cd $var
 
-mv $HOME/hadoop-3.2.4.tar.gz $HOME/sw
+# mv $HOME/hadoop-3.2.4.tar.gz $HOME/sw
 cd $HOME/sw
 tar -xvf hadoop-3.2.4.tar.gz
-mv hadoop-3.2.4 hadoop
+mv hadoop-3.2.4 ../hadoop
 
 
