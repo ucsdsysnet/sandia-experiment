2bb9383d8401af656cf077c364138ff40281e167
rukshani <rukshani@localhost>
2022-08-29 14:45:17 -0600

    Configure hibench

--- workloads/hibench/configure_hibench.sh
+++ workloads/hibench/configure_hibench.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+. workloads/hibench/configurations.config
+WORKER_NODES=$worker_nodes
+KEY_FILE=$key_file
+
+# Duplicate Config Files on Each Worker Node
+IFS=',' read -ra WORKER <<< "$WORKER_NODES"
+for i in "${WORKER[@]}"; do
+        eval `ssh-agent -s`
+        ssh-add $HOME/.ssh/$KEY_FILE
+        scp -r $HOME/hadoop/etc/hadoop/* $USER@$i:$HOME/hadoop/etc/hadoop/
+done
