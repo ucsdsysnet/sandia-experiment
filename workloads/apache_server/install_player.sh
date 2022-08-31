0ca1bdd96dfa5c6ddcd5ee8ea81e1de2d34e7d2c
rukshani <rukshani@localhost>
2022-08-31 12:37:32 -0600

    Install Player

--- workloads/apache_server/install_player.sh
+++ workloads/apache_server/install_player.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# sudo apt update
+sudo apt update
 
-# sudo apt-get install xvfb
+sudo apt-get install xvfb
 
-# sudo apt-get install -y libappindicator1 fonts-liberation ffmpeg
-# cd /tmp
-# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-# sudo dpkg -i google-chrome*.deb
-# sudo apt-get install -f
-# rm google-chrome-stable_current_amd64.deb
+sudo apt-get install -y libappindicator1 fonts-liberation ffmpeg
+cd /tmp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+sudo apt-get install -f
+rm google-chrome-stable_current_amd64.deb
 
 # Testing 
 # xvfb-run google-chrome http://google.com
