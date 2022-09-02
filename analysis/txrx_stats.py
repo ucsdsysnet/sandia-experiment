4169bd959dd60172b3d202d4c4c26996014d6bad
rukshani <rukshani@localhost>
2022-09-02 11:30:12 -0600

    txrx stats

--- analysis/txrx_stats.py
+++ analysis/txrx_stats.py
@@ -12,4 +12,7 @@ all_analysers = az.get_experiment_analysers(f"*{sys.argv[1]}*", sys.argv[2], sys
 for analyser_id in all_analysers:
     print("~~~~~",analyser_id, "~~~~~")
     analyzer = populate_analyzer(all_analysers[analyser_id])
-    print(analyzer.df_client_txrx_stats)
+    
+    analyzer.df_client_txrx_stats['sent_tx_packets'] = analyzer.df_client_txrx_stats['end_tx'] - analyzer.df_client_txrx_stats['start_tx']
+    analyzer.df_client_txrx_stats['sent_rx_packets'] = analyzer.df_client_txrx_stats['end_rx'] - analyzer.df_client_txrx_stats['start_rx']
+    print(analyzer.df_client_txrx_stats.head(35))
